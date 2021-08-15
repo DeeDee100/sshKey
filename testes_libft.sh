2d3d516137866a68b9cf7132d01cd00a37651b31
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2021-08-15 00:50:54 -0300

    Create testes_libft.sh

---
+++ testes_libft.sh
@@ -0,0 +1,22 @@
+bash ssh.sh
+cd ~
+git clone  libft
+cd libft
+git clone https://github.com/ska42/libft-war-machine.git libftTester
+echo
+echo War Machine clonado
+echo
+echo
+git clone https://github.com/Tripouille/libftTester.git Tripoulle
+echo Tripoulle clonado
+cd ~
+git clone https://github.com/alelievr/libft-unit-test.git lunit_teste
+echo
+echo unit-test clonado
+echo
+echo
+echo repo de testes clonado.
+echo
+echo
+echo Fim do script.
+export PS1="\u:\w$ " && bash
