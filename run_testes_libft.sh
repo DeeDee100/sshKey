a71eba46ed2d44a8b6de758d7d1cc8b3905c8126
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2021-08-15 00:51:14 -0300

    Create run_testes_libft.sh

---
+++ run_testes_libft.sh
@@ -0,0 +1,23 @@
+echo Inicio dos testes
+echo
+echo
+cd libft
+cd libftTester
+bash grademe.sh
+echo 'Inicio LibftTester - War_Machine'
+sleep 2
+bash grademe.sh
+echo
+echo
+cd ~
+cd lunit
+echo 'Inicio lunit_teste'
+sleep 2
+echo
+echo
+make f
+echo
+echo
+cd ~
+
+echo 'Fim do Script'
